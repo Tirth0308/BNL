53dec0dd326fafc63a950a5af59a81718ad1eb41
Tirthoraj <mca10057.21@bitmesra.ac.in>
2022-05-29 01:30:52 +0530

    NEW UPDATED

--- register.php
+++ register.php
@@ -1,1 +1,25 @@
 
+<?php
+$server = "localhost";
+$username = "root";
+$password = "";
+$database = "bnl_promoter";
+
+$conn = mysqli_connect($server ,$username,$password,$database);
+
+
+$sname = $_POST['name'];
+$mail = $_POST['email'];
+$pass = $_POST['password'];
+
+$sql = "INSERT INTO promoter(name,email,password) VALUES('$sname','$mail','$pass')";
+
+if(mysqli_query($conn,$sql)==true){
+	echo "<h2>Data inserted Successfully<h2>";
+	echo "<h3>Redirecting to Login in 5 Seconds<h3>";
+	header('refresh: 5; url=Login.html');
+}else
+{
+	echo "Data insertion failed";
+}
+?>
