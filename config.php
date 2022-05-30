8fddb7015c1f296a524813754fd9013636902513
Tirthoraj <mca10057.21@bitmesra.ac.in>
2022-05-31 00:58:37 +0530

    UPDATE

--- config.php
+++ config.php
@@ -3,8 +3,16 @@
 $server = "localhost";
 $username = "root";
 $password = "";
-$database = "";
+$database = "bnl_promoter";
 
 $conn = mysqli_connect($server,$username,$password,$database);
+if($conn)
+{
+	echo "</h3>Connected</h3>";
+}
+else
+{
+	echo "</h3>Not Connected</h3>";
+}
 
 ?>
--- disp.php
+++ disp.php
@@ -1,19 +1,41 @@
 <?php
-
-$server = "localhost";
-$username = "root";
-$password = "";
-$database = "bnl_promoter";
-
-$conn = mysqli_connect($server ,$username,$password,$database);
-$id = $_GET['id'];
-$sql = "SELECT f_name,l_name,uid FROM prom_details WHERE id = '$id'";
-
-$result = mysqli_query($conn,$sql);
-$row = mysqli_fetch_assoc($result);
-
-echo "<tr><td>$row['f_name']</td></tr>";
-echo "<h3>$row['l_name']</h3>";
-echo "<h3>$row['uid']</h3>";
-
+//error_reporting(0);
+include('config.php');
+/* $query = "SELECT * FROM promoter"; */
+$result = mysqli_query($conn,"SELECT * FROM promoter");
 ?>
+<html>
+<head>
+<title>Page Title</title>
+</head>
+<body>
+
+	<table align = "center" border = "2px" style = "width:600px; line-height : 40px;">
+		<tr>
+			<t>
+				<th>id</th>
+				<th>name</th>
+				<th>email</th>
+				<th>password</th>
+			</t>
+		</tr>
+		
+		<?php 
+			while($row=mysqli_fetch_assoc($result))
+			{
+		?>
+		<tr>
+			<td><?php echo $row['id'];?></td>
+			<td><?php echo $row['name'];?></td>
+			<td><?php echo $row['email'];?></td>
+			<td><?php echo $row['password'];?></td>
+		</tr>
+		<?php
+			}
+		?>
+	</table>
+
+
+
+</body>
+</html>
