b0a40b85e7d1cf930eb2cf066cc3d7fc77f10c7d
vatsal <vatsalsinha75@gmail.com>
2022-07-05 12:54:56 +0530

    backend

--- register.php
+++ register.php
@@ -3,17 +3,19 @@
 $server = "localhost";
 $username = "root";
 $password = "";
-$database = "bnl_promoter";
+$database = "bnl";
 
 $conn = mysqli_connect($server ,$username,$password,$database);
-
-
-$sname = $_POST['name'];
+$mem_id = $_POST['memberId'];
+$name = $_POST['name'];
+$mem_type =$_POST['mem_type'];
 $mail = $_POST['email'];
+$gender = $_POST['gender'];
+$phone = $_POST['phone'];
 $pass = $_POST['password'];
 
-$sql = "INSERT INTO promoter(name,email,password) VALUES('$sname','$mail','$pass')";
 
+$sql = "INSERT INTO all_member(member_id,name,mem_type,email,phone,gender,pwd)  VALUES('$mem_id','$name','$mem_type', '$mail','$gender' , '$phone' , '$pass')";
 if(mysqli_query($conn,$sql)==true){
 	echo "Data inserted Successfully";
 }else
--- signin.php
+++ signin.php
@@ -3,23 +3,29 @@
 $server = "localhost";
 $username = "root";
 $password = "";
-$database = "bnl_promoter";
+$database = "bnl";
 
 $conn = mysqli_connect($server ,$username,$password,$database);
-
+//******************************************************************************************************************
 
 if($_SERVER["REQUEST_METHOD"] == "POST") {
 	$mail = $_POST['email'];
 	$pass = $_POST['password'];
-
-	$sql = "SELECT id FROM promoter WHERE email = '$mail' and password = '$pass'";
-	
+    
+	$sql = "SELECT member_id FROM all_member WHERE email = '$mail' and pwd = '$pass' ";
+	$typo = $_POST['typw'];
     $result = mysqli_query($conn,$sql);
     $row = mysqli_fetch_array($result,MYSQLI_ASSOC);	
 	$count = mysqli_num_rows($result);
 	
 		if($count == 1) {	 
-		 header("location: index.html");
+		 // header("location: index.html");
+			if($typo == "Director")
+				echo "Hello Director";
+			else if($typo == 'Customer')
+				echo "Hello customer";
+			else
+				echo "good night";
 		}
 		else {
          echo "Invalid Login Credentials";
